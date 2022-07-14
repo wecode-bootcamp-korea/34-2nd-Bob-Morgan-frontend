b01f806b031264fc3d5e0a4f4a652391f54843c9
dduddu92 <suyun107@gmail.com>
2022-07-14 11:13:51 +0900

    ADD : Footer 레이아웃 완성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterWrap>
+      <FooterLogo src="/images/bobMorgan-logo.png" alt="밥먹언 로고" />
+      <FooterText>
+        Copyright Bob Morgan(밥먹언). All rights reserved.
+      </FooterText>
+    </FooterWrap>
+  );
 };
 
+const FooterWrap = styled.footer`
+  position: relative;
+  padding: 22px 0;
+  text-align: center;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #ddd;
+  }
+`;
+
+const FooterText = styled.p`
+  font-size: 14px;
+  line-height: 1.4em;
+  color: #f69d3e;
+`;
+
+const FooterLogo = styled.img`
+  height: 65px;
+  margin-bottom: 8px;
+  line-height: 0;
+`;
 export default Footer;
